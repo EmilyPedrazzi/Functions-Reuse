130b48b8ef538597805b3457eda307ebeddf3963
EmilyPedrazzi <165744302+EmilyPedrazzi@users.noreply.github.com>
2024-04-05 08:39:17 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+# CS 162 Git Assignment
+
+# Project requires TWO functions:
+# 1. rect_area (length, width) which will return the area of a rectangle
+# 2. rect_solid_area (length, width, height) which will return the area of a solid rectangular object
