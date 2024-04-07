c7e40bd5d36e005117e961a3f17acef3f49d6c08
EmilyPedrazzi <165744302+EmilyPedrazzi@users.noreply.github.com>
2024-04-07 08:07:43 -0700

    Add files via upload

---
+++ lorenzo_ramogsbragg.py
@@ -0,0 +1,14 @@
+#Lorenzo Ramos-Bragg
+#Function 1
+def rect_area(length, width):
+  return length * width
+
+# Request the dimension of a solid rectangular object
+length = int(input("Enter the length of the the object as in integer: "))
+width = int(input("Enter the width of the the object as in integer: "))
+height = int(input("Enter the height of the the object as in integer: "))
+
+surface_area = rect_solid_area(length, width, height)
+
+print("Length = ", length, "Width = ", width, "Height = ", height)
+print("Total Surface Area = ", surface_area)
